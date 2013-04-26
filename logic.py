e77f5821843209b221c520bf85c192e72dde3c1a
Caleb Smith <csmith@caktusgroup.com>
2013-04-26 15:51:49 -0400

    Moved game logic to logic module to clear up game_loop in main

---
+++ logic.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+def logic(game_state, config, level, player):
+    if game_state.is_state('main'):
+        player_coordinate = player.x, player.y
+        for item in level.item_coordinates:
+            if player_coordinate == item['coordinates']:
+                level.item_coordinates.remove(item)
+                item_obj = [x for x in level.items if x.id == item['id']][0]
+                game_state.popup_item(player, item_obj)
+
+        ticks = pygame.time.get_ticks()
+        for monster in level.monsters:
+            if ticks > (monster.last_moved_at + config.monster_delay):
+                monster.last_moved_at = ticks
+                monster.move(level, player)
+                if (monster.x, monster.y) == (player.x, player.y):
+                    game_state.popup_question()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-import pygame
-
 from loaders import initialize, load_level
+from logic import logic
 from graphics import render
 
 
@@ -16,22 +15,7 @@ def main():
 def game_loop(game_state, dispatcher, screen, config, level, player):
     while game_state.state != 'exit':
         dispatcher.handle_events(config, level, player)
-        if game_state.is_state('main'):
-            player_coordinate = player.x, player.y
-            for item in level.item_coordinates:
-                if player_coordinate == item['coordinates']:
-                    level.item_coordinates.remove(item)
-                    item_obj = [x for x in level.items if x.id == item['id']][0]
-                    game_state.popup_item(player, item_obj)
-
-            ticks = pygame.time.get_ticks()
-            for monster in level.monsters:
-                if ticks > (monster.last_moved_at + config.monster_delay):
-                    monster.last_moved_at = ticks
-                    monster.move(level, player)
-                    if (monster.x, monster.y) == (player.x, player.y):
-                        game_state.popup_question()
-
+        logic(game_state, config, level, player)
         render(game_state, screen, config, level, player)
 
 if __name__ == "__main__":
