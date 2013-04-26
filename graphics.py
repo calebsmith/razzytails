eebd245243e5b32898311358c07cb3fdf289bb13
Vinod Kurup <vinod@kurup.com>
2013-04-26 13:46:47 -0400

    Change dialog to popup

--- graphics.py
+++ graphics.py
@@ -53,7 +53,7 @@ def display_player(game_state, screen, config, level, player):
 
 
 def display_monsters(game_state, screen, config, level, player):
-    # don't let monsters move during dialog boxes
+    # don't let monsters move during popup boxes
     if game_state.state != 'main':
         return
     map_width, map_height = level.map.dimensions['width'], level.map.dimensions['height']
@@ -136,11 +136,11 @@ def render(game_state, screen, config, level, player):
             config.questions.get_question_display()
         )
     if game_state.is_state('splash'):
-        dialog_width = config.popup_box['char_width']
+        popup_width = config.popup_box['char_width']
         splash_lines = config.splash_lines
         splash_text = []
         for line in splash_lines:
-            splash_text.extend(word_wrap(line, dialog_width))
+            splash_text.extend(word_wrap(line, popup_width))
             splash_text.append(' ')
         draw_popup(
             game_state, screen, config, level, player,
